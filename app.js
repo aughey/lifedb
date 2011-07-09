3638aef3a16f4b03b0cac079141736e3946742ae
John Aughey <jha@aughey.com>
2011-07-09 13:18:51 -0500

    formatting

--- app.js
+++ app.js
@@ -7,64 +7,64 @@ var express = require('express');
 var mongo = require('mongodb');
 
 function startApp(db,collection) {
-  var app = module.exports = express.createServer();
+	var app = module.exports = express.createServer();
 
-  // Configuration
+	// Configuration
 
-  app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-  });
+	app.configure(function(){
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'jade');
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(express.compiler({ src: __dirname + '/public', enable: ['sass'] }));
+		app.use(app.router);
+		app.use(express.static(__dirname + '/public'));
+	});
 
-  app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-  });
+	app.configure('development', function(){
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	});
 
-  app.configure('production', function(){
-    app.use(express.errorHandler()); 
-  });
+	app.configure('production', function(){
+		app.use(express.errorHandler()); 
+	});
 
-  // Routes
+	// Routes
 
-  app.get('/', function(req, res){
-    res.redirect("/idearank.html");
-    /*
-    res.render('index', {
-      title: 'Express 2'
-    });
-    */
-  });
+	app.get('/', function(req, res){
+		res.redirect("/idearank.html");
+		/*
+			 res.render('index', {
+			 title: 'Express 2'
+			 });
+			 */
+	});
 
-  function executeSingleCommand(command, res) {
-    executeCommand([command],0,{}, [], function(ret) {
-      if(ret.length != 1) {
-        console.log("Really bad, the return value length of executeCommand through ExecuteSingleCommand was not 1");
-      } else {
-        ret = ret[0];
-      }
-      res.send(JSON.stringify(ret));
-    });
-  }
+	function executeSingleCommand(command, res) {
+		executeCommand([command],0,{}, [], function(ret) {
+			if(ret.length != 1) {
+				console.log("Really bad, the return value length of executeCommand through ExecuteSingleCommand was not 1");
+			} else {
+				ret = ret[0];
+			}
+			res.send(JSON.stringify(ret));
+		});
+	}
 
-  function getRecordFromCommand(prefix, command, callback) {
-    var mid;
-    if(command[prefix + "rawid"]) {
-      mid = command[prefix + 'rawid']
-    } else if(command[prefix + "id"]) {
-      mid = db.bson_serializer.ObjectID.createFromHexString(command[prefix + 'id']);
-    } else {
-      callback(null);
-      return;
-    }
-    collection.findOne({_id: mid}, function(err, results) {
-      callback(results);
-    });
-  }
+	function getRecordFromCommand(prefix, command, callback) {
+		var mid;
+		if(command[prefix + "rawid"]) {
+			mid = command[prefix + 'rawid']
+		} else if(command[prefix + "id"]) {
+			mid = db.bson_serializer.ObjectID.createFromHexString(command[prefix + 'id']);
+		} else {
+			callback(null);
+			return;
+		}
+		collection.findOne({_id: mid}, function(err, results) {
+			callback(results);
+		});
+	}
 
 	function bucketobject(command,id) {
 		command.bucket = command.bucket.replace(/\./,'');
@@ -74,17 +74,17 @@ function startApp(db,collection) {
 		return addtoset;
 	}
 
-  function executeCommand(commands, index, state, returndata, donecallback) {
-    if(index >= commands.length) {
-      donecallback(returndata);
-      return;
-    }
-    var command = commands[index];
-    var next = function() {
-      executeCommand(commands,index+1,state,returndata,donecallback);
-    }
-    if(command.op == 'new') {
-      var time = new Date;
+	function executeCommand(commands, index, state, returndata, donecallback) {
+		if(index >= commands.length) {
+			donecallback(returndata);
+			return;
+		}
+		var command = commands[index];
+		var next = function() {
+			executeCommand(commands,index+1,state,returndata,donecallback);
+		}
+		if(command.op == 'new') {
+			var time = new Date;
 
 			if(!command.bucket) {
 				returndata.push("error: bucket not given");
@@ -92,34 +92,34 @@ function startApp(db,collection) {
 				return;
 			}
 
-      getRecordFromCommand("parent_",command, function(parent_record) {
-        if(!parent_record) {
-          returndata.push("error: no parent for " + JSON.stringify(command));
-          donecallback(returndata);
-          return;
-        }
-        var tosave = {
-          data: command.data,
-          created_on: time,
-          parents: [ parent_record._id ]
-        };
-        collection.insert(tosave, function(err, docs) {
-          if(err) {
-            returndata.push("error");
-            donecallback(returndata);
-            return;
-          } else {
+			getRecordFromCommand("parent_",command, function(parent_record) {
+				if(!parent_record) {
+					returndata.push("error: no parent for " + JSON.stringify(command));
+					donecallback(returndata);
+					return;
+				}
+				var tosave = {
+					data: command.data,
+				created_on: time,
+				parents: [ parent_record._id ]
+				};
+				collection.insert(tosave, function(err, docs) {
+					if(err) {
+						returndata.push("error");
+						donecallback(returndata);
+						return;
+					} else {
 						docs = docs[0];
-						
+
 						var addtoset = bucketobject(command,docs._id);
 						collection.update( { _id: parent_record._id }, { '$addToSet' : addtoset }, function() {
 							state.lastsave = docs;
 							returndata.push(docs);
 							next();
 						});
-          }
-        });
-      });
+					}
+				});
+			});
 		} else if(command.op == 'children') {
 			getRecordFromCommand("",command,function(record) {
 				if(!record.children) {
@@ -201,48 +201,48 @@ function startApp(db,collection) {
 		var commandstr = req.param('commands');
 		commandjs = JSON.parse(commandstr);
 
-    var returndata = [ ];
-    var state = { };
-    executeCommand(commandjs,0,state,returndata,function() {
+		var returndata = [ ];
+		var state = { };
+		executeCommand(commandjs,0,state,returndata,function() {
 			res.send(JSON.stringify(returndata));
 		});
-  }
-  app.post("/execute", postfunction);
-  app.get("/execute", postfunction);
+	}
+	app.post("/execute", postfunction);
+	app.get("/execute", postfunction);
 
-  app.get('/get', function(req, res) {
-    executeSingleCommand( { op: 'get', id: req.param('id') }, res);
-  });
-  app.get('/search', function(req, res)  {
-    var q = req.param('q');
-    executeSingleCommand( { op: 'search', q: q }, res);
-  });
-  app.post('/update', function(req, res)  {
-    var id = req.param('id');
-    var mid = db.bson_serializer.ObjectID.createFromHexString(id);
-    var datajs = JSON.parse(req.param('data'));
-    collection.update({_id: mid},{ $set: { data: datajs }},function(err, results) {
-      sendID(collection,mid,res);
-    });
-  });
+	app.get('/get', function(req, res) {
+		executeSingleCommand( { op: 'get', id: req.param('id') }, res);
+	});
+	app.get('/search', function(req, res)  {
+		var q = req.param('q');
+		executeSingleCommand( { op: 'search', q: q }, res);
+	});
+	app.post('/update', function(req, res)  {
+		var id = req.param('id');
+		var mid = db.bson_serializer.ObjectID.createFromHexString(id);
+		var datajs = JSON.parse(req.param('data'));
+		collection.update({_id: mid},{ $set: { data: datajs }},function(err, results) {
+			sendID(collection,mid,res);
+		});
+	});
 
 
-  app.listen(3000);
+	app.listen(3000);
 
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 }
 
 var db = new mongo.Db('lifedb', new mongo.Server('localhost', 27017, {}), {});
 db.open(function(err, db) {
-    db.collection('lifedb', function(err, collection) {
-      collection.insert({_id: 'lifedb'}, function() {
-        if(err) {
-        console.log("Error getting collection");
-        } else {
-        console.log('Connectecd to mongodb lifedb');
-        startApp(db,collection);
-        }
-        });
-      });
-    });
+	db.collection('lifedb', function(err, collection) {
+		collection.insert({_id: 'lifedb'}, function() {
+			if(err) {
+				console.log("Error getting collection");
+			} else {
+				console.log('Connectecd to mongodb lifedb');
+				startApp(db,collection);
+			}
+		});
+	});
+});
 
